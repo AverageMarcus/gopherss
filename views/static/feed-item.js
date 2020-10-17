eb38440ee4d78f2616311b3cfcda9eb3f7ac74b0
Marcus Noble <github@marcusnoble.co.uk>
2020-10-17 19:50:13 +0100

    Improve darkmode

--- views/static/feed-item.js
+++ views/static/feed-item.js
@@ -9,21 +9,36 @@ class FeedItem extends HTMLElement {
     const template = document.createElement('template');
     template.innerHTML = `
     <style>
-    :root {
-      width: 100%;
-      overflow: scroll;
-      overflow-x: auto;
+    :host {
+      width: 100% !important;
+      overflow: scroll !important;
+      overflow-x: auto !important;
     }
     * {
-      max-width: 100%;
-      height: auto;
+      max-width: 100% !important;
+      height: auto !important;
     }
     table {
-      width: 100%;
+      width: 100% !important;
     }
     img {
-      margin: auto auto;
+      margin: auto auto !important;
     }
+    p {
+      font-family: 'Roboto', sans-serif;
+      font-size: 14px;
+      line-height: 20px;
+      letter-spacing: 0em;
+      font-weight: 500;
+    }
+    a {
+      color: #333;
+      font-weight: bold;
+    }
+    :host(.dark) a {
+      color: #ccc;
+    }
+
     </style>
     `;
 
